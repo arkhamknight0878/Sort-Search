d76fb16999092ffa3bcc7552f9ebcf32cae15338
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-05-17 19:42:57 +0300

    Functions.h

--- Buble Sort/Functions.h
+++ Buble Sort/Functions.h
@@ -1,7 +1,7 @@
 #pragma once
 
-int* Rand_Arr_Create(unsigned int size_);
+int* Rand_Arr_Create(unsigned size_);
 
-void Arr_Print(int* arr_, unsigned int size_);
+void Arr_Print(int* arr_, unsigned size_);
 
-void Sort_Buble(int* arr_, unsigned int size_);
+void Sort_Buble(int* arr_, unsigned size_);
