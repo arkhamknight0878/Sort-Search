a47e3899d23ace535606d0eebe30b9c611213267
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-05-17 19:21:44 +0300

    Update Functions.c

--- Binary Search/Functions.c
+++ Binary Search/Functions.c
@@ -24,7 +24,7 @@ unsigned int* Arr_Create(unsigned int size_)
 void Arr_Print(unsigned int* arr_, unsigned int size_)
 {
 	if (!arr_)
-		return NULL;
+		return;
 
 	for (unsigned int i = 0; i < size_; ++i)
 		printf("%2u.%u\n", i, arr_[i]);
@@ -33,7 +33,7 @@ void Arr_Print(unsigned int* arr_, unsigned int size_)
 int Search_Binary(unsigned int* arr_, unsigned int size_, unsigned int key_)
 {
 	if (!arr_)
-		return NULL;
+		return -2;
 
 	unsigned int left = 0;
 	unsigned int right = size_;
