634cd6d1f6b6d6e558ebea891d66005bcec8eb40
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-05-17 19:41:22 +0300

    Functions.h

--- Binary Search/Functions.h
+++ Binary Search/Functions.h
@@ -1,7 +1,7 @@
 #pragma once
 
-unsigned int* Arr_Create(unsigned int size_);
+int* Arr_Create(unsigned size_);
 
-void Arr_Print(unsigned int* arr_, unsigned int  size_);
+void Arr_Print(int* arr_, unsigned  size_);
 
-int Search_Binary(unsigned int* arr_, unsigned int size_, unsigned int key_);
+int Search_Binary(int* arr_, unsigned size_, unsigned key_);
